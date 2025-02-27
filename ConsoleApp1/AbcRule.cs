54f1e976fc5dec276059e0a35aea7dcbe71ddd04
codeghems <emalin.choi@live.com>
2025-02-27 12:19:15 +0800

    Add files via upload

---
+++ ConsoleApp1/AbcRule.cs
@@ -0,0 +1,29 @@
+﻿
+using NRules.Fluent.Dsl;
+
+namespace NRulesSample
+{
+    public class AbcRule : Rule
+    {
+        public override void Define()
+        {
+            Guid AbcId = default!;
+            IEnumerable<Abc> abcCollection = default!;
+
+            When()
+                .Match(() => AbcId)
+                .Query(() => abcCollection, k => k
+                    .Match<Abc>(a => a.AbcId == AbcId)
+                    .Collect());
+
+            Then()
+                .Do(ctx => ctx.Insert(ProcessResult(AbcId, abcCollection)));
+        }
+
+        private AbcResult ProcessResult(Guid abcId, IEnumerable<Abc> abcCollection)
+        {
+            var count = abcCollection.Count();
+            return new AbcResult();
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using NRules;
+using NRules.Fluent;
+using NRulesSample;
+
+
+
+var repository = new RuleRepository();
+repository.Load(x => x.From(typeof(AbcRule).Assembly));
+
+var factory = repository.Compile();
+var session = factory.CreateSession();
+
+var id = new Guid("450912D8-DB17-494B-B229-B6F12B5D51A2");
+
+IEnumerable<Abc> abcCollection =
+[
+    new Abc{AbcId = new Guid("FEE363FB-B33F-478B-A556-DD089475CD83"), Name = "Hello1"},
+    new Abc{AbcId = new Guid("FEE363FB-B33F-478B-A556-DD089475CD83"), Name = "Hello2"},
+    new Abc{AbcId = new Guid("450912D8-DB17-494B-B229-B6F12B5D51A2"), Name = "Hello3"}
+];
+
+session.Insert(id);
+session.InsertAll(abcCollection);
+
+session.Fire();
